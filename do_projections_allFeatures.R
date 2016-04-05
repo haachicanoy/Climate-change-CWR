88e06244a71837c364c9ad71c4dec9a3ea486b9f
haachicanoy <harold22010@gmail.com>
2016-04-05 15:33:51 -0500

    Returning to cwr climate change

---
+++ do_projections_allFeatures.R
@@ -0,0 +1,188 @@
+# Do projections for MaxEnt runs
+# H. Achicanoy
+# CIAT, 2015
+
+make.projections <- function(k,taxon)
+{
+  cat('\n\nProcessing:', spName[taxon], 'on fold', k-1, '\n\n')
+  # lambdas.file <- strsplit(x=fit@models[[k]]@lambdas,split=',',fixed=TRUE)
+  setwd('/curie_data/ncastaneda/gap-analysis/gap_avena/maxent_modeling/models/Avena_abyssinica/crossval/')
+  lambdas.file <- readLines(paste(spName[taxon], '_', k-1, '.lambdas', sep='')) # Read lambdas file by fold
+  lambdas.file <- strsplit(x=lambdas.file, split=',', fixed=TRUE)
+  lambdas.file <- lapply(1:length(lambdas.file),function(i){z <- data.frame(t(lambdas.file[[i]])); return(z)})
+  identify.lmd <- unlist(lapply(1:length(lambdas.file),function(i){z <- ncol(lambdas.file[[i]])==4; return(z)}))
+  paramet.file <- lambdas.file[!identify.lmd]
+  lambdas.file <- lambdas.file[identify.lmd]
+  paramet.file <- Reduce(function(...) rbind(..., deparse.level=1), paramet.file)
+  lambdas.file <- Reduce(function(...) rbind(..., deparse.level=1), lambdas.file)
+  names(paramet.file) <- c("variable","value")
+  names(lambdas.file) <- c("feature","lambda","min","max")
+  
+  lambdas.file$feature <- as.character(lambdas.file$feature)
+  lambdas.file$lambda <- as.numeric(as.character(lambdas.file$lambda))
+  lambdas.file$min <- as.numeric(as.character(lambdas.file$min))
+  lambdas.file$max <- as.numeric(as.character(lambdas.file$max))
+  
+  paramet.file$variable <- as.character(paramet.file$variable)
+  paramet.file$value <- as.numeric(as.character(paramet.file$value))
+  
+  # Identify each type of feature
+  t.feat <- grep(pattern="<", x=lambdas.file$feature, fixed=T) # Threshold features
+  r.feat <- grep(pattern="`", x=lambdas.file$feature, fixed=T) # Reverse hinge features
+  f.feat <- grep(pattern="'", x=lambdas.file$feature, fixed=T) # Forward hinge features
+  q.feat <- grep(pattern="^2", x=lambdas.file$feature, fixed=T) # Quadratic features
+  p.feat <- grep(pattern="*", x=lambdas.file$feature, fixed=T) # Product features
+  l.feat <- setdiff(1:nrow(lambdas.file), c(t.feat, r.feat, f.feat, q.feat, p.feat)) # Linear features
+  
+  # Read climate variables to project
+  temp.dt <- climData[[taxon]][[1]][[1]]
+  temp.dt <- ff(1,dim=c(ncell(temp.dt),20),vmode="double")
+  lapply(1:20,function(i){z <- climData[[taxon]][[1]][[i]]; t <- getValues(z); cat('Processing: biovariable',i,'\n'); temp.dt[,i] <- t[]; return(cat("Done\n"))})
+  temp.dt <- as.ffdf(temp.dt)
+  names(temp.dt) <- paste0("variable.",1:20) # names(temp.dt) <- paste0("bio_",1:20)
+  temp.dt <- as.data.frame(temp.dt)
+  temp.dt <- data.table(temp.dt)
+  
+  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+  # Linear features
+  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+  
+  # Verify if linear features exist
+  if(length(l.feat)>0)
+  {
+    linear.calcs <- lapply(lambdas.file$feature[l.feat], function(var)
+    {
+      lambdas.file.l <- lambdas.file[l.feat,]
+      eval(parse(text=paste('result.by.var.l <- lambdas.file.l$lambda[which(lambdas.file.l$feature==var)]*((temp.dt[,',var,']-lambdas.file.l$min[which(lambdas.file.l$feature==var)])/(lambdas.file.l$max[which(lambdas.file.l$feature==var)]-lambdas.file.l$min[which(lambdas.file.l$feature==var)]))',sep='')))
+      result.by.var.l <- data.frame(result.by.var.l)
+      return(result.by.var.l)
+    })
+    linear.calcs <- Reduce(function(...) cbind(..., deparse.level=1), linear.calcs)
+    names(linear.calcs) <- lambdas.file$feature[l.feat]
+    linear.calcs <- as.data.table(linear.calcs)
+    linear.calcs <- linear.calcs[,rowSums(.SD,na.rm=FALSE)]
+  } else {
+    cat('Linear features does not exist.\n')
+  }
+  
+  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+  # Quadratic features
+  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+  
+  # Verify if quadratic features exist
+  if(length(q.feat)>0)
+  {
+    temp.name <- strsplit(x=lambdas.file$feature[q.feat],split="^",fixed=TRUE)
+    temp.name <- unlist(lapply(temp.name,function(set){return(set[1])}))
+    lambdas.file$feature[q.feat] <- temp.name; rm(temp.name)
+    quadratic.calcs <- lapply(lambdas.file$feature[q.feat],function(var)
+    {
+      lambdas.file.q <- lambdas.file[q.feat,]
+      eval(parse(text=paste('result.by.var.q <- lambdas.file.q$lambda[which(lambdas.file.q$feature==var)]*((temp.dt[,',var,']^2-lambdas.file.q$min[which(lambdas.file.q$feature==var)])/(lambdas.file.q$max[which(lambdas.file.q$feature==var)]-lambdas.file.q$min[which(lambdas.file.q$feature==var)]))',sep='')))
+      result.by.var.q <- data.frame(result.by.var.q)
+      return(result.by.var.q)
+    })
+    quadratic.calcs <- Reduce(function(...) cbind(..., deparse.level=1), quadratic.calcs)
+    names(quadratic.calcs) <- lambdas.file$feature[q.feat]
+    quadratic.calcs <- as.data.table(quadratic.calcs)
+    quadratic.calcs <- quadratic.calcs[,rowSums(.SD,na.rm=FALSE)]
+  } else {
+    cat('Quadratic features does not exist.\n')
+  }
+  
+  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+  # Product features
+  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+  
+  # Verify if product features exist
+  if(length(p.feat)>0)
+  {
+    product.calcs <- lapply(lambdas.file$feature[p.feat],function(comb.var)
+    {
+      lambdas.file.p <- lambdas.file[p.feat,]
+      eval(parse(text=paste('result.by.var.p <- lambdas.file.p$lambda[which(lambdas.file.p$feature==comb.var)]*((temp.dt[,',comb.var,']-lambdas.file.p$min[which(lambdas.file.p$feature==comb.var)])/(lambdas.file.p$max[which(lambdas.file.p$feature==comb.var)]-lambdas.file.p$min[which(lambdas.file.p$feature==comb.var)]))',sep='')))
+      result.by.var.p <- data.frame(result.by.var.p)
+      return(result.by.var.p)
+    })
+    product.calcs <- Reduce(function(...) cbind(..., deparse.level=1), product.calcs)
+    names(product.calcs) <- lambdas.file$feature[p.feat]
+    product.calcs <- as.data.table(product.calcs)
+    product.calcs <- product.calcs[,rowSums(.SD,na.rm=FALSE)]
+  } else {
+    cat('Product features does not exist.\n')
+  }
+  
+  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+  # Forward hinge features
+  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+  
+  if(length(f.feat)>0)
+  {
+    forward.calcs <- lapply(lambdas.file$feature[f.feat], function(var)
+    {
+      lambdas.file.f <- lambdas.file[f.feat,]
+      eval(parse(text=paste('result.by.var.p <- lambdas.file.p$lambda[which(lambdas.file.p$feature==comb.var)]*((temp.dt[,',comb.var,']-lambdas.file.p$min[which(lambdas.file.p$feature==comb.var)])/(lambdas.file.p$max[which(lambdas.file.p$feature==comb.var)]-lambdas.file.p$min[which(lambdas.file.p$feature==comb.var)]))',sep='')))
+      result.by.var.p <- data.frame(result.by.var.p)
+      return(result.by.var.p)
+    })
+    product.calcs <- Reduce(function(...) cbind(..., deparse.level=1), product.calcs)
+    names(product.calcs) <- lambdas.file$feature[p.feat]
+    product.calcs <- as.data.table(product.calcs)
+    product.calcs <- product.calcs[,rowSums(.SD,na.rm=FALSE)]
+  } else {
+    cat('Product features does not exist.\n')
+  }
+  
+  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+  # Reverse hinge features
+  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+  
+  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+  # Threshold features
+  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+  
+  
+  if(exists('linear.calcs') & exists('quadratic.calcs') & exists('product.calcs')) # Linear, quadratic and product
+  {
+    fx.calcs <- cbind(linear.calcs,quadratic.calcs,product.calcs); rm(linear.calcs,quadratic.calcs,product.calcs)
+  } else {
+    if(exists('linear.calcs') & exists('quadratic.calcs') & !exists('product.calcs')) # Linear and quadratic
+    {
+      fx.calcs <- cbind(linear.calcs,quadratic.calcs); rm(linear.calcs,quadratic.calcs)
+    } else {
+      if(exists('linear.calcs') & !exists('quadratic.calcs') & exists('product.calcs')) # Linear and product
+      {
+        fx.calcs <- cbind(linear.calcs,product.calcs); rm(linear.calcs,product.calcs)
+      } else {
+        if(exists('linear.calcs') & !exists('quadratic.calcs') & !exists('product.calcs')) # Linear only
+        {
+          cat('Projections do not available because model ran with linear features only.\n')
+        }
+      }
+    }
+  }
+  
+  if(exists('fx.calcs')) # Verify if fx.calcs exists in order to calculate projections
+  {
+    
+    fx.calcs <- as.data.table(fx.calcs)
+    fx.calcs <- fx.calcs[,rowSums(.SD,na.rm=FALSE)]
+    
+    S.x <- fx.calcs - paramet.file$value[which(paramet.file$variable=="linearPredictorNormalizer")]
+    Q.x <- exp(S.x)/paramet.file$value[which(paramet.file$variable=="densityNormalizer")]
+    L.x <- (Q.x*exp(paramet.file$value[which(paramet.file$variable=="entropy")]))/(1+Q.x*exp(paramet.file$value[which(paramet.file$variable=="entropy")]))
+    
+    # First index corresponds to taxon information, Second index corresponds to climatic information model
+    prj_fn <- climData[[taxon]][[1]][[1]]
+    cell <- 1:ncell(prj_fn)
+    prj_fn[cell] <- L.x
+    
+    return(prj_fn)
+    
+  } else {
+    
+    return(cat('Process finished for inaccurate results.\n'))
+    
+  }
+  
+}
